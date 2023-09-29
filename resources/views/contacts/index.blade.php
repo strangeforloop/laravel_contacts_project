a09571f46a49770ad21f081c21d6ddf006dc4d8e
strangeforloop <agohaanna@gmail.com>
2023-09-29 01:49:53 -0700

    revert styling bc validation errors shifting

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -10,81 +10,92 @@
   <body>
     <div>
       <h1 for="body">New Contact Info</h1>
-        <form action="{{ route('contacts') }}" method="post" style="margin-bottom:2rem">
-          @csrf
-          <div style="display:flex;flex-direction:row">
-            <div style="display:flex;flex-direction:column">
-              <label for="name" style="margin-right:10px;padding:1.5px">Name</label>
-              <label for="email" style="margin-right:10px;padding:1.5px">Email</label>
-              <label for="phone" style="margin-right:10px;padding:1.5px">Phone</label>
-              <label for="city" style="margin-right:10px;padding:1.5px">City</label>
-              <label for="country" style="margin-right:10px;padding:1.5px">Country</label>
-              <label for="state" style="margin-right:10px;padding:1.5px">State</label>
-              <label for="zip_code" style="margin-right:10px;padding:1.5px">Zip Code</label>
-            </div>
+      <div style="display:flex;flex-direction:row">
+      <div style="display:flex;flex-direction:column">
+        <label for="name" style="margin-right:10px;padding:1.5px">Name</label>
+        <label for="email" style="margin-right:10px;padding:1.5px">Email</label>
+        <label for="phone" style="margin-right:10px;padding:1.5px">Phone</label>
+        <label for="city" style="margin-right:10px;padding:1.5px">City</label>
+        <label for="country" style="margin-right:10px;padding:1.5px">Country</label>
+        <label for="state" style="margin-right:10px;padding:1.5px">State</label>
+        <label for="zip_code" style="margin-right:10px;padding:1.5px">Zip Code</label>
+      </div>
+      <form action="{{ route('contacts') }}" method="post" style="margin-bottom:2rem">
+        @csrf
+        <div style="display:flex;flex-direction:row">
+          <!-- <div style="display:flex;flex-direction:column">
+            <label for="name" style="margin-right:10px;padding:1.5px">Name</label>
+            <label for="email" style="margin-right:10px;padding:1.5px">Email</label>
+            <label for="phone" style="margin-right:10px;padding:1.5px">Phone</label>
+            <label for="city" style="margin-right:10px;padding:1.5px">City</label>
+            <label for="country" style="margin-right:10px;padding:1.5px">Country</label>
+            <label for="state" style="margin-right:10px;padding:1.5px">State</label>
+            <label for="zip_code" style="margin-right:10px;padding:1.5px">Zip Code</label>
+          </div> -->
+          <div>
             <div>
-              <div>
-                <input type="text" name="name" id="name" placeholder="Name" value="">
-                @error('name')
+              <input type="text" name="name" id="name" placeholder="Name" value="">
+              @error('name')
+                  {{ $message }}
+                </div>
+              @enderror
+            </div>
+
+            <div>
+                <input type="text" name="email" id="email" placeholder="Email" value="">
+                @error('email')
                     {{ $message }}
                   </div>
                 @enderror
-              </div>
-
-              <div>
-                  <input type="text" name="email" id="email" placeholder="Email" value="">
-                  @error('email')
-                      {{ $message }}
-                    </div>
-                  @enderror
-              </div>
-
-              <div>
-                  <input type="text" name="phone" id="phone" placeholder="Phone" value="">
-                  @error('phone')
-                      {{ $message }}
-                    </div>
-                  @enderror
-              </div>
-
-              <div>
-                  <input type="text" name="city" id="city" placeholder="City" value="">
-                  @error('city')
-                      {{ $message }}
-                    </div>
-                  @enderror
-              </div>
-
-              <div>
-                  <input type="text" country="country" id="country" placeholder="Country" value="">
-                  @error('country')
-                      {{ $message }}
-                    </div>
-                  @enderror
-              </div>
-
-              <div>
-                  <input type="text" name="state" id="state" placeholder="State" value="">
-                  @error('state')
-                      {{ $message }}
-                    </div>
-                  @enderror
-              </div>
-
-              <div>
-                  <input type="text" name="zip_code" id="zip_code" placeholder="Zip Code" value="">
-                  @error('zip_code')
-                      {{ $message }}
-                    </div>
-                  @enderror
-              </div>
             </div>
-            
+
+            <div>
+                <input type="text" name="phone" id="phone" placeholder="Phone" value="">
+                @error('phone')
+                    {{ $message }}
+                  </div>
+                @enderror
+            </div>
+
+            <div>
+                <input type="text" name="city" id="city" placeholder="City" value="">
+                @error('city')
+                    {{ $message }}
+                  </div>
+                @enderror
+            </div>
+
+            <div>
+                <input type="text" country="country" id="country" placeholder="Country" value="">
+                @error('country')
+                    {{ $message }}
+                  </div>
+                @enderror
+            </div>
+
+            <div>
+                <input type="text" name="state" id="state" placeholder="State" value="">
+                @error('state')
+                    {{ $message }}
+                  </div>
+                @enderror
+            </div>
+
+            <div>
+                <input type="text" name="zip_code" id="zip_code" placeholder="Zip Code" value="">
+                @error('zip_code')
+                    {{ $message }}
+                  </div>
+                @enderror
+            </div>
           </div>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
+          
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
         </form>
+      </div>
     
       @if ($contacts->count())
       <div style="max-width:60%">
